5118c397f37533a85773fa220cc64e7e2e422564
georgegathu <ggeorge.gathu@gmail.com>
2022-09-12 22:49:53 +0300

    Format Specifiers

---
+++ FormatSpecifiers.c
@@ -0,0 +1,9 @@
+Different Format Specifiers to use for different types:
+1. %d   - int = -10;
+2. %ud  - unsigned int = ; 
+3. %ld  - long c = -10; 
+4. %lu  - unsigned long d = 20;
+5. %f   - float e = 10.2;
+6. %lf  - double f = 200.2;
+7. %c   - char g = 'a';
+8. %s   - char str[] = "abc";
